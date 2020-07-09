59e4992984d16a222bdf31e332a6c1fbda5e0602
Douile <25043847+Douile@users.noreply.github.com>
2020-07-09 03:16:12 +0100

    Update help to work with aliases

--- src/commands/help.js
+++ src/commands/help.js
@@ -12,25 +12,42 @@ const matchAny = function(text, search) {
 const call = async function(message, parts) {
   const search = parts.map(s => new RegExp(s, 'gi'));
   if (search.length === 0) {
+    const matched = {};
     await message.channel.send(new RichEmbed({
       title: 'Help',
       description: Array.from(message.client.commands.values())
-        .filter(cmd => cmd.name !== 'help' && (isOfBaseType(cmd.check, Function) ? cmd.check(message) : true))
-        .map(cmd => `\`${message.client.config.prefix}${cmd.name}\``)
+        .filter(cmd => {
+          // This is kinda hacky I should store a names only command object
+          if (cmd.name !== 'help' && (isOfBaseType(cmd.check, Function) ? cmd.check(message) : true)) {
+            if (cmd.name in matched) return false;
+            matched[cmd.name] = true;
+            return true;
+          }
+          return false;
+        })
+        .map(cmd => cmd.alias.map(alias => `\`${message.client.config.prefix}${alias}\``).join(', '))
         .join('\n'),
       footer: { text: `Use "${message.client.config.prefix}help commandName" for detailed help` }
     }));
   } else {
-    const matches = Array.from(message.client.commands.values())
-      .filter(cmd => matchAny(`${message.client.config.prefix}${cmd.name}`, search) && (isOfBaseType(cmd.check, Function) ? cmd.check(message) : true));
+    const matched = {};
+    const matches = Array.from(message.client.commands.entries())
+      .filter(([alias,cmd]) => {
+        if (matchAny(`${message.client.config.prefix}${alias}`, search) && (isOfBaseType(cmd.check, Function) ? cmd.check(message) : true)) {
+          if (cmd.name in matched) return false;
+          matched[cmd.name] = true;
+          return true;
+        }
+        return false;
+      });
     await message.channel.send(new RichEmbed({
       title: 'Help',
       description: matches.length === 0 ? `Sorry no commands matched the pattern \`${parts.join(' ')}\` (_commands you don't have access to will not appear_)\nYou can list all the commands you have access to with \`${message.client.config.prefix}help\`` : undefined,
       fields:
-        matches.map(cmd => {
+        matches.map(([alias, cmd]) => {
           return {
             name: `${message.client.config.prefix}${cmd.name}`,
-            value: isOfBaseType(cmd.help, String) ? cmd.help.replace(/\{command\}/gi, `${message.client.config.prefix}${cmd}`) : 'No help message provided',
+            value: isOfBaseType(cmd.help, String) ? cmd.help.replace(/\{command\}/gi, `${message.client.config.prefix}${cmd.name}`) : 'No help message provided',
             inline: false
           };
         })
